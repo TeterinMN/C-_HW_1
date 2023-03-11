b755aa21a068d0aea876a5f6b71d1f3f199140dc
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-11 18:42:44 +0900

    completed the first task

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b) Console.WriteLine("MAX = " + a);
+else Console.WriteLine("MAX = " + b);
