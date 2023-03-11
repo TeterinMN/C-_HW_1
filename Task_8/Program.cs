d669ea48f2439c14a034c90806a07193ff412ad7
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-11 19:39:51 +0900

    completed task 8

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+while (i <= num)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"Все чётные числа от 1 до {num} = {i}");
+    }
+    i++;
+}
