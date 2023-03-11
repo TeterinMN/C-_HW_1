5db705b38a95806723ea5ec3cd9f71fce026d88b
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-11 19:21:50 +0900

    completed task 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+    Console.WriteLine($"Число является четным {number}");
+else Console.WriteLine($"Число является HE четным {number}");
