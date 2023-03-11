b33dfe7471a8c165f57ef9dbd19b027af932f4cc
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-11 19:14:13 +0900

    completed task 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b && a > c)
+    Console.WriteLine($"Максимальное число {a}");
+else if (b > a && b > c)
+    Console.WriteLine($"Максимальное число {b}");
+else
+    Console.WriteLine($"Максисмальное число {c}");
